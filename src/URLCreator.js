b793364490b69188c20c7fb69f2cc18fb35dad3c
Mark Salabutin <salabutinmark4work@gmail.com>
2019-03-27 20:36:27 +0200

    Add test for urlCreator

--- src/URLCreator.js
+++ src/URLCreator.js
@@ -1,15 +1,22 @@
-export default ({ scopes, appId, authServerURI }) => {
-  const scopes = JSON.stringify(scopes);
+const initURLCreator = (options) => {
+  if (!options) {
+    throw Error('Options have to be an object.')
+  }
+
+  const { scopes, appId, authServerURI } = options;
+  const scopesJSON = JSON.stringify(scopes);
 
   return Object.freeze({
     createLoginURL() {
-      return `${authServerURI}/web/login?appId=${appId}&scopes=${scopes}`
+      return `${authServerURI}/web/login?appId=${appId}&scopes=${scopesJSON}`
     },
     createRenewSessionURL() {
-      return `${authServerURI}/web/token/renew?appId=${appId}&scopes=${scopes}`;
+      return `${authServerURI}/web/token/renew?appId=${appId}&scopes=${scopesJSON}`;
     },
     createRegistrationURL() {
-      return `${authServerURI}/web/register?appId=${appId}&scopes=${scopes}`
+      return `${authServerURI}/web/register?appId=${appId}&scopes=${scopesJSON}`
     },
   });
 };
+
+export default initURLCreator;
