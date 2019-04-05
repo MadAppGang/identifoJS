bafd943d4dcd0fefb41037e1144ccb29f0ecaea2
Mark Salabutin <salabutinmark4work@gmail.com>
2019-04-05 13:09:55 +0300

    Rename Iframe method

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const init = function (options) {
     const iframe = Iframe.create();
 
     const timeout = setTimeout(() => {
-      Iframe.delete(iframe);
+      Iframe.remove(iframe);
       reject(TimeoutExpiredError);
     }, 30000);
 
@@ -52,7 +52,7 @@ const init = function (options) {
 
         const body = tokenService.decode(token);
 
-        Iframe.delete(iframe);
+        Iframe.remove(iframe);
         resolve({ token, body });
       });
   });
