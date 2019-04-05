73876df00ad49c59825ac49ed3c0b22fcb5f2319
Mark Salabutin <salabutinmark4work@gmail.com>
2019-04-05 12:36:00 +0300

    hotfix

--- src/URLCreator.js
+++ src/URLCreator.js
@@ -1,15 +1,15 @@
 export default ({ scopes, appId, authServerURI }) => {
-  const scopes = JSON.stringify(scopes);
+  const scopesJSON = JSON.stringify(scopes);
 
   return Object.freeze({
     createLoginURL() {
-      return `${authServerURI}/web/login?appId=${appId}&scopes=${scopes}`
+      return `${authServerURI}/web/login?appId=${appId}&scopes=${scopesJSON}`
     },
     createRenewSessionURL() {
-      return `${authServerURI}/web/token/renew?appId=${appId}&scopes=${scopes}`;
+      return `${authServerURI}/web/token/renew?appId=${appId}&scopes=${scopesJSON}`;
     },
     createRegistrationURL() {
-      return `${authServerURI}/web/register?appId=${appId}&scopes=${scopes}`
+      return `${authServerURI}/web/register?appId=${appId}&scopes=${scopesJSON}`
     },
   });
 };
