e03e9f71b1ef36a898bfab397f890afbcda808e0
Mark Salabutin <salabutinmark4work@gmail.com>
2019-08-13 16:53:52 +0300

    Add callback url to the request urls and updated tests

--- src/URLCreator.js
+++ src/URLCreator.js
@@ -3,21 +3,21 @@ const initURLCreator = (options) => {
     throw Error('Options have to be an object.')
   }
 
-  const { scopes, appId, authServerURI } = options;
+  const { scopes, appId, authServerURI, callbackURL } = options;
   const scopesJSON = JSON.stringify(scopes);
 
   return Object.freeze({
     createLoginURL() {
-      return `${authServerURI}/web/login?appId=${appId}&scopes=${scopesJSON}`
+      return `${authServerURI}/web/login?appId=${appId}&scopes=${scopesJSON}&callbackURL=${callbackURL}`
     },
     createRenewSessionURL() {
-      return `${authServerURI}/web/token/renew?appId=${appId}&scopes=${scopesJSON}`;
+      return `${authServerURI}/web/token/renew?appId=${appId}&scopes=${scopesJSON}&callbackURL=${callbackURL}`;
     },
     createRegistrationURL() {
-      return `${authServerURI}/web/register?appId=${appId}&scopes=${scopesJSON}`
+      return `${authServerURI}/web/register?appId=${appId}&scopes=${scopesJSON}&callbackURL=${callbackURL}`
     },
     createLogoutURL() {
-      return `${authServerURI}/web/logout?appId=${appId}&scopes=${scopesJSON}`;
+      return `${authServerURI}/web/logout?appId=${appId}&scopes=${scopesJSON}&callbackURL=${callbackURL}`;
     }
   });
 };
--- src/optionsValidation.js
+++ src/optionsValidation.js
@@ -4,6 +4,7 @@ import {
 
 const optionsRules = {
   authServerURI: [requiredRule, makeTypeValidationRule({ type: 'string' }), URLValidationRule],
+  callbackURL: [requiredRule, makeTypeValidationRule({ type: 'string' }), URLValidationRule],
   appId: [requiredRule, makeTypeValidationRule({ type: 'string' })],
   scopes: [requiredRule, makeTypeValidationRule({ type: 'array' })],
 };
